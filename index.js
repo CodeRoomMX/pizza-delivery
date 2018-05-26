0780445143b1aa4c3e1a02290ff927afbc21c264
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-05-26 11:06:17 -0500

    Challenge accepted

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const colors = require('colors');
 const cmd = require('commander');
 const mongoose = require('mongoose');
 const Pizza = require('./db/Pizza');
-//let pedidos = [];
 
 mongoose.connect('mongodb://localhost/pizza-cats-test')
   .then(instance => {
     const conn = instance.connections[0];
-    //console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
   })
   .catch(err => {
     console.error(`ERROR: ${err.message}`);
@@ -17,24 +16,16 @@ mongoose.connect('mongodb://localhost/pizza-cats-test')
   });
 
 cmd
-  .command('add <flavour> <size>')
-  .option('-c, --cash', 'Pay with cash')
-  .option('-d, --delivery', 'Add included delivery')
+  .command('order')
   .action(function(flavour, size, opts){
-    console.log()
-    Pizza.create({flavour, size, cash: opts.cash, delivery: opts.delivery}).then(function(pizza){
+    console.log(flavour, size, opts);
+    Pizza.create().then(function(){
       mongoose.disconnect();
     });
-    //pedidos.push({flavour, size, delivery});
-    console.log('Pizza created!!!');
   });
 
-  cmd
-  .command('list')
-  .action(function(){
-    Pizza.find()
-    .then(pizzas => pizzas.map(pizza => console.log(`${pizza.flavour}  | ${pizza.size} | ${pizza.cash?'Pago en Efectivo'?'Pago en TDC'} | ${pizza.delivery?'Envío incluido':'Sin envio'}`)))
-    
-  });
+  
+  //Pizza.find()
+  //  .then(pizzas => )
 
 cmd.parse(process.argv);
